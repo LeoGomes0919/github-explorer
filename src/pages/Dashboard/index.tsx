91c5cc11914958663144e2f57f2e13db48eb5942
LeoGomes0919 <leonardogs0919@gmail.com>
2020-11-09 22:21:16 -0300

    Atualização do projeto, melhoria no layout.

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,115 @@
-import React from 'react';
+import React, { useState, FormEvent, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { FiChevronRight } from 'react-icons/fi';
+
+import { Header, Main, Form, Repositories, Error } from './styles';
+import logoImage from '../../assets/github-logo.svg';
+import api from '../../services/api';
+import Repository from '../Repository';
+
+interface Repository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
 
 const Dashboard: React.FC = () => {
-  return <h1>Dashboard</h1>;
+  const [inputRepo, setInputRepo] = useState('');
+  const [inputError, setInputError] = useState('');
+  const [repositories, setRepositories] = useState<Repository[]>(() => {
+    const storangedRpositories = localStorage.getItem(
+      '@GihubExplorer:repository',
+    );
+
+    if (storangedRpositories) {
+      return JSON.parse(storangedRpositories);
+    }
+    return [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem(
+      '@GihubExplorer:repository',
+      JSON.stringify(repositories),
+    );
+  }, [repositories]);
+
+  async function handleAddRepo(
+    event: FormEvent<HTMLFormElement>,
+  ): Promise<void> {
+    event.preventDefault();
+    if (!inputRepo) {
+      setInputError('Digite o autor/nome do repositório.');
+      return;
+    }
+
+    const existRepository = localStorage
+      .getItem('@GihubExplorer:repository')
+      ?.toUpperCase()
+      ?.indexOf(inputRepo.toUpperCase());
+
+    if (existRepository !== -1) {
+      setInputError('Esse repositório já existe na lista');
+      setTimeout(() => {
+        setInputError('');
+      }, 5000);
+      return;
+    }
+
+    try {
+      const response = await api.get(`repos/${inputRepo}`);
+
+      const repository = response.data;
+
+      setRepositories([...repositories, repository]);
+      setInputRepo('');
+      setInputError('');
+    } catch (error) {
+      setInputError('Erro na busca por esse repositório.');
+    }
+  }
+
+  return (
+    <>
+      <Header>
+        <img src={logoImage} alt="Logo" />
+      </Header>
+      <Main>
+        <header>
+          <h1>Explore repositórios no Github</h1>
+        </header>
+
+        <Form hasError={!!inputError} onSubmit={handleAddRepo}>
+          <input
+            onChange={(e) => setInputRepo(e.target.value)}
+            placeholder="Digite o nome do repositório"
+            value={inputRepo}
+          />
+          <button type="submit">Pesquisar</button>
+        </Form>
+        {inputError && <Error>{inputError}</Error>}
+        <Repositories>
+          {repositories.map((repository) => (
+            <Link key={repository.full_name} to="/repository">
+              <img
+                src={repository.owner.avatar_url}
+                alt={repository.owner.login}
+              />
+
+              <div>
+                <strong>{repository.full_name}</strong>
+                <span>{repository.description}</span>
+              </div>
+              <FiChevronRight size={20} />
+            </Link>
+          ))}
+        </Repositories>
+      </Main>
+    </>
+  );
 };
 
 export default Dashboard;
---
+++ src/pages/Dashboard/styles.ts
@@ -0,0 +1,182 @@
+import styled, { css } from 'styled-components';
+import { shade } from 'polished';
+
+interface FormProps {
+  hasError: boolean;
+}
+
+export const Header = styled.header`
+  h1 {
+    font-size: 32px;
+  }
+`;
+
+export const Main = styled.main`
+  header {
+    margin-top: 80px;
+    max-width: 450px;
+
+    h1 {
+      font-size: 48px;
+      line-height: 58px;
+      color: #3a3a3a;
+    }
+  }
+
+  @media (max-width: 768px) {
+    width: 100%;
+
+    header {
+      h1 {
+        font-size: 32px;
+        line-height: 42px;
+      }
+    }
+  }
+`;
+
+export const Form = styled.form<FormProps>`
+  margin-top: 40px;
+  max-width: 700px;
+  display: flex;
+
+  input {
+    min-height: 50px;
+    flex: 1;
+    border: 0;
+    padding: 0 24px;
+    border-radius: 4px 0 0 4px;
+    transition: all 0.2s;
+    color: #3a3a3a;
+    border: 1px solid #fff;
+    border-right: 0;
+
+    ${(props) =>
+      props.hasError &&
+      css`
+        border-color: #ff4040;
+      `}
+
+    &::placeholder {
+      color: #a8a8b3;
+    }
+  }
+
+  button {
+    height: 50px;
+    width: 210px;
+    border: 0;
+    background: #04d361;
+    border-radius: 0px 4px 4px 0px;
+    color: #fff;
+    font-weight: bold;
+    transition: all 0.2s;
+
+    &:hover {
+      background: ${shade(0.2, '#04d361')};
+    }
+  }
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+    width: 100%;
+
+    input {
+      border-radius: 4px;
+    }
+
+    button {
+      margin-top: 16px;
+      border-radius: 4px;
+      width: 100%;
+    }
+  }
+`;
+
+export const Error = styled.span`
+  display: block;
+  margin-top: 8px;
+  color: #ff4040;
+`;
+
+export const Repositories = styled.div`
+  max-width: 700px;
+  margin: 80px 0 40px 0;
+
+  a {
+    display: flex;
+    align-items: center;
+    text-decoration: none;
+    background: #fff;
+    border-radius: 4px;
+    padding: 8px 16px;
+    transition: 0.2s;
+    box-shadow: 2px 4px 8px -2px rgba(0, 0, 0, 0.2);
+
+    &:hover {
+      transform: translateX(8px);
+      border: 1px solid #04d361;
+
+      svg {
+        color: #04d361;
+      }
+    }
+
+    + a {
+      margin-top: 16px;
+    }
+  }
+
+  img {
+    width: 52px;
+    height: 52px;
+    border-radius: 50%;
+  }
+
+  div {
+    display: flex;
+    flex-direction: column;
+    margin-left: 16px;
+
+    strong {
+      font-size: 16px;
+      color: #3d3d3d;
+      line-height: 26px;
+    }
+
+    span {
+      font-size: 14px;
+      color: #a8a8b3;
+      line-height: 24px;
+    }
+  }
+
+  svg {
+    margin-left: auto;
+    color: #cbcbd6;
+  }
+`;
+
+export const Pagination = styled.div`
+  max-width: 700px;
+  padding: 4px 0px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const PaginationItem = styled.div`
+  border: 1px solid #d3d3d3;
+  padding: 8px 16px;
+  border-radius: 2px;
+  color: #3d3d3d;
+  cursor: pointer;
+
+  & + {
+    margin-left: 4px;
+  }
+
+  &:hover {
+    background: #ddd;
+  }
+`;
